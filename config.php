7e9d710c59644f56ed18dd74c142bda359c72778
Dmitri Popov <dmpop@linux.com>
2022-03-10 10:05:35 +0100

    Improve login

--- config.php
+++ config.php
@@ -8,11 +8,11 @@ $inum = 7;
 //Enable password protection
 $protect = true;
 // Password
-$passwd = 'monkey';
+$password = 'monkey';
 // Timezone
 $tz = 'Europe/Berlin';
 // Footer
-$footer = "Read the <a href='https://dmpop.gumroad.com/l/php-right-away'>PHP Right Away</a> book";
+$footer = "I really 🧡 <a href='https://www.paypal.com/paypalme/dmpop'>coffee</a>";
 // Openweathermap API key
 $api_key = "ee3752672ae1423b9bb92919e2b51e97";
 ?>
--- login.php
+++ login.php
@@ -1,51 +1,50 @@
 <?php
 $config = include('config.php');
+$pw_hash = password_hash($password, PASSWORD_DEFAULT);
 
-/* Redirects here after login */
-$redirect_after_login = 'index.php';
+if ($protect) {
+	session_start();
+}
 
-/* Set timezone to UTC */
-
-date_default_timezone_set('UTC');
-
-/* Will not ask password again for */
-$remember_password = strtotime('+30 days'); // 30 days
-
-if (isset($_POST['password']) && $_POST['password'] == $passwd) {
-	setcookie("password", $passwd, $remember_password);
-	header('Location: ' . $redirect_after_login);
+if (isset($_POST['password']) && password_verify($_POST['password'], $pw_hash)) {
+	$_SESSION["password"] = $pw_hash;
+	if (file_exists(".page")) {
+		$page = file_get_contents('.page');
+		$url = "index.php?page=" . $page;
+	} else {
+		$url = "index.php";
+	}
+	header('Location: ' . $url);
 	exit;
 }
 ?>
-<!DOCTYPE html>
+
 <html lang="en" data-theme="<?php echo $theme ?>">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title><?php echo $title ?></title>
-<link rel="shortcut icon" href="favicon.png" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="classless.css" />
-<title><?php echo $title ?></title>
+
+<head>
+	<title><?php echo $title; ?></title>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="favicon.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/classless.css" />
+	<link rel="stylesheet" href="css/themes.css" />
+	<link rel="stylesheet" href="css/tabbox.css" />
 </head>
 
 <body>
-
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title><?php echo $title ?></title>
-		<link rel="shortcut icon" href="favicon.png" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/classless.css" />
-		<link rel="stylesheet" href="css/themes.css" />
-		<div class="card text-center" style="margin-top: 0em;">
-			<img style="display: inline; height: 2em; vertical-align: middle;" src="favicon.png" alt="logo" />
-			<h1 class="text-center" style="display: inline; margin-left: 0.19em; vertical-align: middle; letter-spacing: 3px; margin-top: 0em;"><?php echo $title ?></h1>
-			<hr style="margin-bottom: 1em;">
-			<form method="POST">
-				<label for='pagename'>Password: </label>
-				<input type="password" name="password">
-				<button type="submit" name="login">Log in</button>
-			</form>
-		</div>
+	<div style="text-align: center;">
+		<img style="display: inline; height: 2em; border-radius: 0; vertical-align: middle;" src="favicon.svg" alt="logo" />
+		<h1 style="display: inline; margin-left: 0.19em; vertical-align: middle; letter-spacing: 3px; margin-top: 0em;"><?php echo $title ?></h1>
+		<hr style="margin-bottom: 2em; margin-top: 1em;">
+		<form method="POST">
+			<label>Password:
+				<input style="margin-top: 1em; display: inline;" type="password" name="password">
+				<button type="submit" name="go">Go</button>
+			</label>
+		</form>
+		<hr style="margin-top: 2em; margin-bottom: 1.5em;">
+		<?php echo $footer; ?>
+	</div>
 </body>
 
 </html>
